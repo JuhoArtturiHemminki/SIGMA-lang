3a156a0ad900d6bca0ad25254b748597d595c55f
Juho Artturi Hemminki <projectflagcarrier@gmail.com>
2026-04-08 00:41:18 +0300

    Delete target directory

--- target/release/deps/sigma_compiler-939849922670c18d.d
+++
@@ -1,5 +0,0 @@
-/Users/1-d.f.s./Desktop/SIGMA-lang/target/release/deps/sigma_compiler-939849922670c18d.d: src/main.rs
-
-/Users/1-d.f.s./Desktop/SIGMA-lang/target/release/deps/sigma_compiler-939849922670c18d: src/main.rs
-
-src/main.rs:
--- target/release/sigma-compiler.d
+++
@@ -1,1 +0,0 @@
-/Users/1-d.f.s./Desktop/SIGMA-lang/target/release/sigma-compiler: /Users/1-d.f.s./Desktop/SIGMA-lang/src/main.rs
